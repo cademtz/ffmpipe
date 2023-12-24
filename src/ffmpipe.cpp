b1843b09be87d1a822287ddd2304a9bc7016a93a
cademtz <cadeemz@outlook.com>
2023-12-23 22:08:02 -0600

    Hide FFmpeg window

--- src/ffmpipe.cpp
+++ src/ffmpipe.cpp
@@ -121,7 +121,7 @@ std::shared_ptr<Pipe> Pipe::Create(
         NULL,               // process security attributes 
         NULL,               // primary thread security attributes 
         TRUE,               // handles are inherited 
-        0,                  // creation flags 
+        CREATE_NO_WINDOW,   // creation flags 
         NULL,               // use parent's environment 
         NULL,               // use parent's current directory 
         &startup_info,      // STARTUPINFO pointer 
